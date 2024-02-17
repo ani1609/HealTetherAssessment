94a90c830fe06ca517b4b7bbb92b980343b96e2b
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2024-02-17 16:55:53 +0530

    setup tweaks

--- server/index.js
+++ server/index.js
@@ -4,11 +4,13 @@ require('dotenv').config();
 
 
 // Define a route that responds with "Hello, World!"
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get('/', (req, res) => 
+{
+    res.send('Hello, World!');
 });
 
 // Start the server
-app.listen(process.env.PORT, () => {
-  console.log(`Server is listening at http://localhost:${process.env.PORT}`);
+app.listen(process.env.PORT, () => 
+{
+    console.log(`Server is listening at http://localhost:${process.env.PORT}`);
 });

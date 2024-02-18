cab615a8d763979bbedc77aaea878f0eaccf2518
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2024-02-19 00:49:03 +0530

    basic post ui stsructure

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,14 +1,39 @@
 import "../index.css";
+import {ReactComponent as Like} from "../icons/like.svg";
+import {ReactComponent as Comment} from "../icons/comment.svg";
+import {ReactComponent as Share} from "../icons/share.svg";
 
 
-function PostCard({ post }) {
+function PostCard({ post }) 
+{
+    function formatTimestamp(timestamp) {
+        const options = { day: '2-digit', month: 'short', year: 'numeric' };
+        const formattedDate = new Date(timestamp).toLocaleDateString('en-US', options);
+        return formattedDate;
+    }
+
     return (
-        <div className="" style={{width: "100%"}}>
-            <div className="flex justify-start gap-x-2">
+        <div className="flex flex-col box-border" style={{width: "100%"}}>
+            <div className="flex justify-start gap-x-2 box-border p-2">
                 <img src={"http://localhost:3001/uploads/profilePictures/default.png"} alt="post" className="w-11" />
-                <h1 className="flex items-center">{post.creator.name.split(' ')[0]}</h1>
+                <div className="flex flex-col justify-center">
+                    <h1 className="text-sm font-semibold">{post.creator.name.split(' ')[0]}</h1>
+                    <p className="text-xs">{formatTimestamp(post.timeStamp)}</p>
+                </div>
             </div>
-            <img src={"http://localhost:3001/uploads/postPictures/"+post.image} alt="post" className="w-full" />
+            <p className="box-border px-2 text-base">{post.caption}</p>
+            <img src={post.imageData} alt="post" className="w-full h-60 object-cover mt-1" />
+            <div className="flex justify-between items-center cursor-pointer mt-1">
+                <span className="flex justify-center box-border p-2 gap-x-2 items-center" style={{ flex: 1 }}>
+                    <Like className="h-6 w-6"/> <p className="text-xs">Like</p>
+                </span>
+                <span className="flex justify-center box-border p-2 gap-x-2 items-center" style={{ flex: 1 }}>
+                    <Comment className="h-5 w-5"/> <p className="text-xs">Comment</p>
+                </span>
+                <span className="flex justify-center box-border p-2 gap-x-2 items-center" style={{ flex: 1 }}>
+                    <Share className="h-5 w-5"/> <p className="text-xs">Share</p>
+                </span>
+            </div>
         </div>
     );
 }
--- server/index.js
+++ server/index.js
@@ -14,8 +14,8 @@ const server=http.createServer(app);
 
 // middlewares
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Connect to MongoDB
 connectDb();

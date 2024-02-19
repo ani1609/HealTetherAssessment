8b482c03f98f5f4f1ee416c5e834fb9a85b456f8
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2024-02-19 23:36:58 +0530

    tweaks

--- client/src/components/Notifications.js
+++ client/src/components/Notifications.js
@@ -4,11 +4,15 @@ import {ReactComponent as Close} from '../icons/close.svg';
 function Notifications(props) 
 {
     
-
+    const hadleCrossClick = () =>
+    {
+        props.setNotifications([]);
+        props.setShowNotification(false);
+    };
 
     return (
         <div className="bg-white relative p-8 shadow-md rounded-md w-80 h-96">
-            <div className="absolute cursor-pointer p-2" style={{right: "10px", top:"10px"}} onClick={()=>props.setShowNotification(false)}><Close className="w-5 h-5"/></div>
+            <div className="absolute cursor-pointer p-2" style={{right: "10px", top:"10px"}} onClick={hadleCrossClick}><Close className="w-5 h-5"/></div>
             <h2 className="flex justify-center text-2xl font-bold mb-6">Notifications</h2>
             <div className="mb-4 border-t">
                 {props.notifications.map((notification, index) => (
